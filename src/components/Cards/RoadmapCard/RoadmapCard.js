e9ef111b44fd28d55991673ef87cd6af3f48831e
abdullahmehboob20s <abdullahmehboob2002@gmail.com>
2022-01-22 21:11:57 +0500

    done

--- src/components/Cards/RoadmapCard/RoadmapCard.js
+++ src/components/Cards/RoadmapCard/RoadmapCard.js
@@ -1,24 +1,61 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styles from "./RoadmapCard.module.css";
 import { useInView } from "react-intersection-observer";
 
-function RoadmapCard({ isActive, phase, title, points }) {
-  const { ref, inView } = useInView({
-    threshold: 1,
-    rootMargin: "100% 0px 0px 0px",
+function RoadmapCard({
+  isActive,
+  phase,
+  title,
+  points,
+  roadmapLine,
+  value,
+  show = false,
+  lastRef = false,
+}) {
+  const circleRef = useRef();
+  // const { ref, inView } = useInView({
+  //   threshold: 1,
+  //   rootMargin: "100% 0px 0px 0px",
+  // });
+  const [inView, setInView] = useState(false);
+
+  useEffect(() => {
+    const handler = () => {
+      if (show) {
+        // let lineHeight = roadmapLine.current.clientHeight;
+        // let windowHieght = window.innerHeight / 2;
+        // let b = circleRef.current.clientHeight / 2;
+        // let circleTop = circleRef.current.offsetTop + b;
+        // if (lineHeight > windowHieght - circleTop) {
+        //   setInView(true);
+        // } else {
+        //   setInView(false);
+        // }
+        // console.log(roadmapLine.current.getBoundingClientRect());
+      }
+    };
+
+    window.addEventListener("scroll", handler);
+
+    return () => {
+      window.removeEventListener("scroll", handler);
+    };
   });
 
   return (
     <div
-      ref={ref}
       className={`${styles.cardWrapper} ${isActive ? styles.active : ""}`}
+      ref={lastRef ? lastRef : null}
     >
       <div className={styles.cardLeft}>
         <p className="fs-56px yellow uppercase font-anybody-bold">
           PHASE {phase}
         </p>
       </div>
-      <div className={`${styles.cardRight} ${inView ? styles.isVisible : ""}`}>
+
+      <div className={`${styles.cardRight} ${styles.isVisible}`}>
+        <div className={styles.cardRightCircle} ref={circleRef}></div>
+
         <div className={styles.cardContent}>
           <h4
             className={`fs-28px yellow font-anybody-bold lh-1_3 ${
--- src/layouts/HomePage/Roadmap/Roadmap.js
+++ src/layouts/HomePage/Roadmap/Roadmap.js
@@ -1,9 +1,30 @@
 import RoadmapCard from "components/Cards/RoadmapCard/RoadmapCard";
 import Title from "components/Title/Title";
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styles from "./Roadmap.module.css";
 
 function Roadmap() {
+  const roadmap = useRef();
+  const roadmapLine = useRef();
+  const [value, setValue] = useState(null);
+  const lastRef = useRef(null);
+
+  useEffect(() => {
+    const handler = () => {
+      let a = window.scrollY - roadmap.current.offsetTop;
+      let value = a + window.innerHeight / 2;
+
+      // roadmapLine.current.style.height = value + "px";
+      setValue(value);
+    };
+
+    window.addEventListener("scroll", handler);
+
+    return () => {
+      window.removeEventListener("scroll", handler);
+    };
+  });
+
   return (
     <>
       <div className="container-wrapper">
@@ -21,8 +42,22 @@ function Roadmap() {
         </p>
       </div>
       <div className="container-wrapper">
-        <div className={styles.roadmap}>
+        <div className={styles.roadmap} ref={roadmap}>
+          <div
+            className={styles.roadmapLine}
+            style={{
+              height: `calc(100% - ${lastRef?.current?.scrollHeight || 0}px)`,
+            }}
+          >
+            <div
+              className={styles.roadmapLineInner}
+              style={{ height: value + "px" }}
+              ref={roadmapLine}
+            ></div>
+          </div>
           <RoadmapCard
+            roadmapLine={roadmapLine}
+            value={value}
             phase="1"
             title="Staking"
             isActive={true}
@@ -32,24 +67,33 @@ function Roadmap() {
             ]}
           />
           <RoadmapCard
+            roadmapLine={roadmapLine}
+            value={value}
             phase="2"
             isActive={false}
             title="New drops to start our 3D Collection Drop"
             points={["3D Avatars", "Mind blowing special 3D collection"]}
+            show={true}
           />
           <RoadmapCard
+            roadmapLine={roadmapLine}
+            value={value}
             phase="3"
             isActive={false}
             title="Launch pre-orders on EKS collection clothing line"
             points={["Clothing collabs", "Rich Cycle Couture"]}
           />
           <RoadmapCard
+            roadmapLine={roadmapLine}
+            value={value}
             phase="4"
             isActive={false}
             title="Short Animation video Launch for the avatas created"
             points={[]}
           />
           <RoadmapCard
+            roadmapLine={roadmapLine}
+            value={value}
             phase="5"
             isActive={false}
             title="Create Rich Cycle NFT & Fashion academy"
@@ -57,10 +101,14 @@ function Roadmap() {
               "This will be the first NFT Academy worldwide; DON'T MISS IT",
             ]}
           />
+
           <RoadmapCard
+            roadmapLine={roadmapLine}
+            value={value}
             phase="6"
             isActive={false}
             title="Create a MMORPG ( Massive Multi Online Role Player Game) game with all avatars combined"
+            lastRef={lastRef}
             points={[
               "This will be the first NFT Academy worldwide; DON'T MISS IT",
             ]}
